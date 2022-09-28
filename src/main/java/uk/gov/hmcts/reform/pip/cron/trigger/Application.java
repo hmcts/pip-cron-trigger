59b8e719dfea402c2d86c6f9d5873d4e93252cc8
ChrisS1512 <Christopher.Stacey@justice.gov.uk>
2022-09-28 15:38:56 +0100

    Created command line runner

--- src/main/java/uk/gov/hmcts/reform/pip/cron/trigger/Application.java
+++ src/main/java/uk/gov/hmcts/reform/pip/cron/trigger/Application.java
@@ -1,13 +1,23 @@
 package uk.gov.hmcts.reform.pip.cron.trigger;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
-public class Application {
+public class Application implements CommandLineRunner {
 
     public static void main(final String[] args) {
+        System.out.println("WE HAVE STARTED THE APP");
         SpringApplication.run(Application.class, args);
+        System.out.println("WE HAVE FINISHED THE APP");
+    }
+
+    @Override
+    public void run(String... args) {
+        System.out.println("WE ARE RUNNING!!");
+        System.out.println("WE ARE FINISHED");
+
     }
 }
