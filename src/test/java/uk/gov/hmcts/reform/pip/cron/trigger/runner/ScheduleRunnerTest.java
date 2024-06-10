afc26127127ad9e0db9474a112b078e1c0bb6697
abottenberg <alexbottenberg@hotmail.com>
2024-06-10 15:23:52 +0100

    PUB-2461: Removing setUp and tearDown from ScheduleRunnerTest

--- src/test/java/uk/gov/hmcts/reform/pip/cron/trigger/runner/ScheduleRunnerTest.java
+++ src/test/java/uk/gov/hmcts/reform/pip/cron/trigger/runner/ScheduleRunnerTest.java
@@ -41,17 +41,6 @@ class ScheduleRunnerTest {
 
     private static final String MESSAGE_DO_NOT_MATCH_MESSAGE = "Messages do not match";
     private static final String STATUS_DO_NOT_MATCH_MESSAGE = "Status codes do not match";
-
-    @Before
-    public void setUp() {
-        System.setProperty("java.security.manager", "allow");
-    }
-
-    @After
-    public void tearDown() {
-        System.clearProperty("java.security.manager");
-    }
-
     @Test
     void testRunnerWhereInvalidArgProvided() throws Exception {
         try (LogCaptor logCaptor = LogCaptor.forClass(ScheduleRunner.class)) {
